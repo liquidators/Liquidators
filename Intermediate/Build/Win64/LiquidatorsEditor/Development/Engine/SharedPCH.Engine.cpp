db2998c2547997e87c5f7cce3a00b0f0e51d156f
ahmadmmsadeghi <ahmadmmsadeghi@gmail.com>
2019-09-11 23:17:02 -0700

    test2
    test2

---
+++ Intermediate/Build/Win64/LiquidatorsEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/ahmad/Desktop/Liquidators-master/Intermediate/Build/Win64/LiquidatorsEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ Intermediate/Build/Win64/UE4Editor/Inc/Liquidators/LiquidatorsGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef LIQUIDATORS_LiquidatorsGameModeBase_generated_h
+#error "LiquidatorsGameModeBase.generated.h already included, missing '#pragma once' in LiquidatorsGameModeBase.h"
+#endif
+#define LIQUIDATORS_LiquidatorsGameModeBase_generated_h
+
+#define Liquidators_master_Source_Liquidators_LiquidatorsGameModeBase_h_15_RPC_WRAPPERS
+#define Liquidators_master_Source_Liquidators_LiquidatorsGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Liquidators_master_Source_Liquidators_LiquidatorsGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesALiquidatorsGameModeBase(); \
+	friend struct Z_Construct_UClass_ALiquidatorsGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ALiquidatorsGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/Liquidators"), NO_API) \
+	DECLARE_SERIALIZER(ALiquidatorsGameModeBase)
+
+
+#define Liquidators_master_Source_Liquidators_LiquidatorsGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesALiquidatorsGameModeBase(); \
+	friend struct Z_Construct_UClass_ALiquidatorsGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ALiquidatorsGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/Liquidators"), NO_API) \
+	DECLARE_SERIALIZER(ALiquidatorsGameModeBase)
+
+
+#define Liquidators_master_Source_Liquidators_LiquidatorsGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ALiquidatorsGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ALiquidatorsGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ALiquidatorsGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ALiquidatorsGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ALiquidatorsGameModeBase(ALiquidatorsGameModeBase&&); \
+	NO_API ALiquidatorsGameModeBase(const ALiquidatorsGameModeBase&); \
+public:
+
+
+#define Liquidators_master_Source_Liquidators_LiquidatorsGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ALiquidatorsGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ALiquidatorsGameModeBase(ALiquidatorsGameModeBase&&); \
+	NO_API ALiquidatorsGameModeBase(const ALiquidatorsGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ALiquidatorsGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ALiquidatorsGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ALiquidatorsGameModeBase)
+
+
+#define Liquidators_master_Source_Liquidators_LiquidatorsGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define Liquidators_master_Source_Liquidators_LiquidatorsGameModeBase_h_12_PROLOG
+#define Liquidators_master_Source_Liquidators_LiquidatorsGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Liquidators_master_Source_Liquidators_LiquidatorsGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Liquidators_master_Source_Liquidators_LiquidatorsGameModeBase_h_15_RPC_WRAPPERS \
+	Liquidators_master_Source_Liquidators_LiquidatorsGameModeBase_h_15_INCLASS \
+	Liquidators_master_Source_Liquidators_LiquidatorsGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Liquidators_master_Source_Liquidators_LiquidatorsGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Liquidators_master_Source_Liquidators_LiquidatorsGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Liquidators_master_Source_Liquidators_LiquidatorsGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Liquidators_master_Source_Liquidators_LiquidatorsGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	Liquidators_master_Source_Liquidators_LiquidatorsGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> LIQUIDATORS_API UClass* StaticClass<class ALiquidatorsGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Liquidators_master_Source_Liquidators_LiquidatorsGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
